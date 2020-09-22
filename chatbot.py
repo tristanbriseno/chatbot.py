8be70feb771eef5f7e5e37075a225f0e85e786c9
tristanbriseno <69834247+tristanbriseno@users.noreply.github.com>
2020-09-22 14:09:21 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@ from random import choice
 def get_rude_bot_response(user_response):
 #Here I am defining that get_rude_bot_response
   
-  bot_response_good = ["Oh, my bad! I didnt mean to make you think I care.", "Alright good talk", "oh, wait sorry, gotta take this call", "Awe man i just remembered, I've got this thing I gotta do, so yeah....Tootles!"]
+  bot_response_good = ["Oh, my bad! I didnt mean to make you think I care.", "Alright good talk", "oh, sorry, gotta take this call", "Awe man i just remembered, I've got this thing I gotta do, so yeah....Tootles!"]
   #here I am making 2 lists of reponses based off of the imput from the user
   bot_response_bad = ["But did you die?", "    Are you always such an idiot, or do you just show off when I’m around?", "Are you about to cry?", ".............Oh did you say somethin?", "ZzzZzzZzzZzzZzzZzz" ]
   #here it is saying that if the user inputs good then respond with bot_response_good and if the user inputs bad then respond with bot_response_bad else return the text provided.
